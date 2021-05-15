deea9774dafa50945b88e7c9f728c3ec27fb7aae
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-05-15 02:52:47 -0500

    update

---
+++ MODULE-6/Javascript.js
@@ -0,0 +1,23 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px";
+
+});
+
+document.getElementById("BlueBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("FadeBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+document.getElementById("ResetBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "25px";
+
+});
